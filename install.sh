77f94f66383738c05094f85186b865c9c59faa58
adamci <adam.cimpeanu@aya.yale.edu>
2017-09-22 16:51:09 -0400

    automate most of the installs

--- install.sh
+++ install.sh
@@ -3,12 +3,22 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 
-# TODO install base16 clone
-#git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
-# TODO install command-not-found
-#brew tap homebrew/command-not-found
-# TODO install diff-so-fancy
-# TODO install icdiff
+# Install base16 clone
+if [[ ! -d ~/.config/base16-shell ]]; then
+    git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
+fi
+# Install command-not-found
+if ! type "command-not-found" > /dev/null 2>&1; then
+    brew tap homebrew/command-not-found
+fi
+# Install diff-so-fancy
+if ! type "diff-so-fancy" > /dev/null 2>&1; then
+    brew install diff-so-fancy
+fi
+# Install icdiff
+if ! type "icdiff" > /dev/null 2>&1; then
+    brew install icdiff
+fi
 
 # Create symbolic links
 which zsh 2>&1 > /dev/null && ln -vis "${DIR}/.zshrc" ~/.zshrc
@@ -16,9 +26,12 @@ which vim 2>&1 > /dev/null && ln -vis "${DIR}/.vimrc" ~/.vimrc
 which git 2>&1 > /dev/null && ln -vis "${DIR}/.gitconfig" ~/.gitconfig
 which tmux 2>&1 > /dev/null && ln -vis "${DIR}/.tmux.conf" ~/.tmux.conf
 
-# TODO install vundle
-#git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-# TODO install vim plugins
-#vim +PluginInstall
+# TODO Check for correct vim version
+# Install vundle
+if [[ ! -a ~/.vim/bundle/Vundle.vim ]]; then
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi 
+# Install vim plugins
+vim +PluginUpdate +qa
 # TODO compile command-t
 # TODO compile YouCompleteMe
