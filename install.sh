14eadc1e6b47d76dcda880b441cea7b84cbe8358
Adam Cimpeanu <adam.cimpeanu@aya.yale.edu>
2018-02-16 07:15:39 +0000

    add command-not-found support to arch

--- install.sh
+++ install.sh
@@ -42,6 +42,14 @@ if [[ ! -a /usr/local/share/antigen/antigen.zsh ]]; then
         sudo ln -s /usr/share/zsh/share/antigen.zsh /usr/local/share/antigen/antigen.zsh
     fi
 fi
+if [[ $OS == "Linux" ]] && ! type "pkgfile" > /dev/null 2>&1; then
+    # pkgfile is required for command-not-found support
+    pacaur -S pkgfile
+    echo "Updating pkgfile"
+    sudo pkgfile -u
+    echo "Enabling pkgfile daily update"
+    systemctl enable --now pkgfile-update.timer
+fi
 if ! type "diff-so-fancy" > /dev/null 2>&1; then
     if [ $OS == "Darwin" ]; then
         brew install diff-so-fancy
