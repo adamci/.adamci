7e100e870b1d980baab5b01b01d30bd04a322127
Adam Cimpeanu <adam.cimpeanu@aya.yale.edu>
2018-12-02 21:37:28 -0500

    add full command-t install

---
+++ deps/command-t.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if ! grep -q "+ruby" <(vim --version); then
+    echo "Vim version must support ruby!"
+    exit 1
+fi
+
+if ! type "ruby" > /dev/null 2>&1; then
+    if [ "$DIST" = "1" ]; then
+        brew install ruby
+    elif [ "$DIST" = "2" ]; then
+        $ARCH_PACMAN -S ruby
+    fi
+fi
+
+cd ~/.vim/bundle/command-t/ruby/command-t/ext/command-t
+ruby extconf.rb
+make
+cd $DIR
--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ if [ $DIR != ~/.adamci ]; then
     exit
 fi
 
+# Validate OS
 source ./identify_os.sh
 if [ "$DIST" = "0" ]; then
     echo $'Unsupported OS\nAborting install_packages.sh!\n'
@@ -17,6 +18,7 @@ if [ "$DIST" = "2" ] && ! type $ARCH_PACMAN > /dev/null 2>&1; then
     exit 1
 fi
 
+# Validate presence of best editor
 if [ ! type "vim" > /dev/null 2>&1 ] || [ "$(vim --version | head -1 | cut -d ' ' -f 5 | head -c 1)" -lt "8" ]; then
     echo "Please install vim at version 8 or higher!"
     exit 1
@@ -30,13 +32,6 @@ if [[ ! -d ~/.config/base16-shell ]]; then
     git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
 fi
 
-source ./deps/antigen.sh
-source ./deps/pkgfile.sh
-source ./deps/diff-so-fancy.sh
-source ./deps/icdiff.sh
-source ./deps/ag.sh
-source ./deps/tmux.sh
-
 # Create symbolic links
 which zsh 2>&1 > /dev/null && ln -vis "${DIR}/.zshrc" ~/.zshrc
 which vim 2>&1 > /dev/null && ln -vis "${DIR}/.vimrc" ~/.vimrc
@@ -47,3 +42,12 @@ ln -vis "${DIR}/.ideavimrc" ~/.ideavimrc
 
 # Install vim plugins
 vim +PluginUpdate +qa
+source ./deps/command-t.sh
+
+# Install apps
+source ./deps/antigen.sh
+source ./deps/pkgfile.sh
+source ./deps/diff-so-fancy.sh
+source ./deps/icdiff.sh
+source ./deps/ag.sh
+source ./deps/tmux.sh
