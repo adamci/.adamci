3c2eab6437f52f2f2e656ac6fb4f59ff649fe6ae
Adam Cimpeanu <adam.cimpeanu@aya.yale.edu>
2018-12-02 20:10:38 -0500

    require vim at version 8 or higher

--- install.sh
+++ install.sh
@@ -17,6 +17,11 @@ if [ "$DIST" = "2" ] && ! type $ARCH_PACMAN > /dev/null 2>&1; then
     exit 1
 fi
 
+if [ ! type "vim" > /dev/null 2>&1 ] || [ "$(vim --version | head -1 | cut -d ' ' -f 5 | head -c 1)" -lt "8" ]; then
+    echo "Please install vim at version 8 or higher!"
+    exit 1
+fi
+
 # Install shell dependencies
 if [[ ! -a ~/.vim/bundle/Vundle.vim ]]; then
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
